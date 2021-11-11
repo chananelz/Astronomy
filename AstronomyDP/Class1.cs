9710dc089e1ec194dd90022b62ec23a937b7cccd
elya kofler <elya.kofler4@gmail.com>
2021-11-11 23:02:44 +0200

    Saving the ID in DP

--- AstronomyDP/Class1.cs
+++ AstronomyDP/Class1.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-// Elya Kofler
+// Elya Kofler 212853139
 // Chananel Zaguri
 
 namespace AstronomyDP
