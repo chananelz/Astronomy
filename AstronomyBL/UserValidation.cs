91ebec5cfe3712bcc5090f4f5400f5ad91bf38fe
elya kofler <elya.kofler4@gmail.com>
2022-04-05 09:16:51 +0300

    Integration

---
+++ AstronomyBL/UserValidation.cs
@@ -0,0 +1,60 @@
+﻿using AstronomyDAL;
+using AstronomyDP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RestSharp;
+
+
+namespace AstronomyBL
+{
+    public class UserValidation
+    {
+        private User_reader UserCollection;
+
+        public UserValidation()
+        {
+            UserCollection = new User_reader();
+        }
+
+        public bool ValidateUser(string Username, string Password)
+        {
+            var Exist = (from MyUser in UserCollection.UsersDetails
+                         where MyUser.UserName == Username && MyUser.Password == Password
+                         select MyUser);
+            return Exist.Any();
+        }
+
+        public bool Sign_up_request(string Username, string Password, string EmailAddress, bool IsInterestedInPictures)
+        {
+            if ((from MyUser in UserCollection.UsersDetails
+                 where MyUser.UserName == Username
+                 select MyUser).Any() == true)
+            {
+                return false;
+            }
+            else if (Password.Length < 6)
+            {
+                return false;
+            }
+            else
+            {
+                User_reader p = new User_reader();
+                User NewUser = new User
+                {
+                    UserName = Username,
+                    Password = Password,
+                    EmailAddress = EmailAddress,
+                    IsIntrestedInEmails = IsInterestedInPictures
+                };
+                
+                p.AddNewUser(NewUser);
+                return true;
+            }
+            
+        }
+    }
+}
---
+++ AstronomyDAL/User_reader.cs
@@ -0,0 +1,30 @@
+﻿using AstronomyDP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AstronomyDAL
+{
+    public class User_reader
+    {
+        public List<User> UsersDetails { get; set; }
+        
+        public User_reader()
+        {
+            using (AstronomyDBEntities context = new AstronomyDBEntities())
+            {
+                UsersDetails = (from U in context.Users
+                                select U).ToList();
+            } 
+           
+        }
+
+        public bool AddNewUser(User user)
+        {
+            UsersDetails.Add(user);
+            return true;
+        }
+    }
+}
