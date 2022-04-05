8fcf0516a1ed02b66c3d7e50e45a6c20866e0f93
chananelz <chananelz@gmail.com>
2022-04-05 21:17:32 +0300

    SetResponse

---
+++ AstronomyDP/Image_Modal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AstronomyDP
+{
+    public class Image_Modal
+    {
+        public string Img { get; set; }
+    }
+}
--- AstronomyWPF/Window2.xaml.cs
+++ AstronomyWPF/Window2.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Drawing;
+using System.IO;
+using AstronomyDP;
+using FireSharp.Response;
 
 namespace AstronomyWPF
 {
@@ -22,6 +27,59 @@ namespace AstronomyWPF
         public Window2()
         {
             InitializeComponent();
+        }
+        public bool ThumbnailCallback()
+        {
+            return false;
+        }
+
+        BitmapImage BitmapToImageSource(Bitmap bitmap)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+
+                return bitmapimage;
+            }
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Select Image";
+            ofd.Filter = "Image Files(*.jpg) | *.jpg";
+
+            if (ofd.ShowDialog() == true)
+            {
+                
+                //System.Windows.Media.ImageSource img = new BitmapSource(ofd.FileName);
+                //pictureBox.Source = img.GetThumbnailImage(240, 161, null, new IntPtr());
+                System.Drawing.Image.GetThumbnailImageAbort myCallback = new System.Drawing.Image.GetThumbnailImageAbort(ThumbnailCallback);
+                Bitmap myBitmap = new Bitmap(ofd.FileName);
+                pictureBox.Source = BitmapToImageSource((Bitmap)myBitmap.GetThumbnailImage(240, 161, null, new IntPtr()));
+
+                byte[] imgData = System.IO.File.ReadAllBytes(ofd.FileName);
+                string output = Convert.ToBase64String(imgData);
+                var data = new Image_Modal
+                {
+                    Img = output
+                };
+                SetResponse response = await client.SetTaskAsync("Image/", data);
+                Image_Modal result = response.ResultAs<Image_Modal>();
+
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+
         }
     }
 }
