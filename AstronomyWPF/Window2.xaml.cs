146c0839371e250f90300da8f0545071e7979a2c
chananelz <chananelz@gmail.com>
2022-04-06 14:22:25 +0300

    all work after fireBase

--- AstronomyWPF/Window2.xaml.cs
+++ AstronomyWPF/Window2.xaml.cs
@@ -16,6 +16,10 @@ using System.Drawing;
 using System.IO;
 using AstronomyDP;
 using FireSharp.Response;
+using FireSharp.Interfaces;
+using FireSharp.Config;
+using FireSharp;
+using Newtonsoft.Json;
 
 namespace AstronomyWPF
 {
@@ -24,6 +28,12 @@ namespace AstronomyWPF
     /// </summary>
     public partial class Window2 : Window
     {
+        IFirebaseConfig fc = new FirebaseConfig
+        {
+            AuthSecret = "gPqWPJP1H6zF7bZo1jQf5ax91slhGLmdPSY8fuvt",
+            BasePath = "https://fir-astronomy-default-rtdb.firebaseio.com/"
+        };
+        IFirebaseClient client;
         public Window2()
         {
             InitializeComponent();
@@ -49,8 +59,10 @@ namespace AstronomyWPF
             }
         }
 
-        private async void Button_Click(object sender, RoutedEventArgs e)
+        private  void Button_Click(object sender, RoutedEventArgs e)
         {
+
+
             OpenFileDialog ofd = new OpenFileDialog();
             ofd.Title = "Select Image";
             ofd.Filter = "Image Files(*.jpg) | *.jpg";
@@ -64,21 +76,27 @@ namespace AstronomyWPF
                 Bitmap myBitmap = new Bitmap(ofd.FileName);
                 pictureBox.Source = BitmapToImageSource((Bitmap)myBitmap.GetThumbnailImage(240, 161, null, new IntPtr()));
 
-                byte[] imgData = System.IO.File.ReadAllBytes(ofd.FileName);
-                string output = Convert.ToBase64String(imgData);
-                var data = new Image_Modal
-                {
-                    Img = output
-                };
-                SetResponse response = await client.SetTaskAsync("Image/", data);
-                Image_Modal result = response.ResultAs<Image_Modal>();
+                //byte[] imgData = System.IO.File.ReadAllBytes(ofd.FileName);
+                //string output = Convert.ToBase64String(imgData);
+                //var data = new Image_Modal
+                //{
+                //    Img = output
+                //};
+                //SetResponse response =  client.Set("Image/" + "dsfs", data);
+                //Image_Modal result = response.ResultAs<Image_Modal>();
 
             }
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
+            var data = new Image_Modal
+                {
+                    Img = "input"
+                };
 
+            client = new FirebaseClient(fc);
+            var seret = client.Set("Image/" + "dfs" , data);
 
         }
     }
